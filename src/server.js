d9a8f9ebe5c022f1ca9e65ca2c4169ca1746fbea
Arvind <jsrarvind12@gmail.com>
2026-03-29 23:40:13 +0530

    done with all chat logic

--- src/server.js
+++ src/server.js
@@ -15,21 +15,41 @@ const socketio = require("socket.io");
 const server = http.createServer(app); // here we are passing express app in server thats why i am calling in bottom server.listen()
 const io = socketio(server);
 
+const Chat = require('../models/chat');
+
+
 io.on("connection", (socket) => {
-  console.log("a user connected.", socket.id);
+//   console.log("a user connected.", socket.id);
+
+socket.on('join_room',(data)=>{
+    socket.join(data.roomId);
+});
+
+
+
 
   // listening messege from the client side --> using socket.on
   // after that you will get msg what client has sent
-  socket.on("msg_send", (data) => {
+  socket.on("msg_send", async(data) => {
     console.log(data);
+    const chat =  await Chat.create({
+        roomId:data.roomId,
+        user:data.username,
+        content:data.msg
+    });
     // socket.emit('msg_rcvd',data); // for everyone
-       socket.broadcase.emit('msg_rcvd',data); // for every one except himself
-    io.emit("msg_rcvd", data); // here server sending same data to all connection on the socket for that we will write script in the script.js
+    // socket.broadcast.emit('msg_rcvd',data); // for every one except himself
+    io.to(data.roomId).emit("msg_rcvd", data); // here server sending same data to all connection on the socket for that we will write script in the script.js
     // ---> IMP ---> means every one on that localhost will get this msg
     // now you can vizualize that from one browser i am sending msg and on secnod browser also getting msg.
     // so if deploy and same same link ko open krr rahe hai to tum paa sakte ho msg ek dusare ka
     // lekin ek problem hai ki sbb log dekh sakte hai,
   });
+
+  socket.on('typing',(data)=>{
+    socket.broadcast.to(data.roomId).emit('someone_typing');
+  });
+
 
   // socket.on('from_client',()=>{
   //     console.log("event comming from client.")
@@ -49,7 +69,20 @@ io.on("connection", (socket) => {
 // const socket = io();  paste this in scrip.js which is using in the index.html
 // ----> after pasting you will see that every time when any one going on that localhost endpoint from any browser on same machin you will see a new id will be created(see in the console.)
 
+
+app.set('view engine','ejs');
+
 app.use("/", express.static(path.join(__dirname, "..", "public")));
+
+
+app.get('/chat/:roomId',async(req,res)=>{
+    const chats = await Chat.find({
+        roomId:req.params.roomId,
+    }).select('content');
+     console.log(chats);
+     res.render('index',{name:'arvind',id:req.params.roomId,chats:chats});
+});
+
 
 //-----> IMP---> here we will use server.listen instead of app.listen because of socket.io
 server.listen(PORT, async() => {
